bd97d3fbe01e263b5632d858f4939388ac9076b4
ABHIJEETKATARE <114878748+ABHIJEETKATARE@users.noreply.github.com>
2024-08-02 10:51:19 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -56,11 +56,11 @@ catch(e){
     res.json({error:e.message});
 }
 
-})
-authRouter.get('/',async(req,res)=>{
-    token=req.header('x-auth-token');
-    verified=jwt.verify(token,"passwordKey");
-    const user = await User.findById(verified.id);
-    res.json({ ...user._doc, token });
 });
+// authRouter.get('/',async(req,res)=>{
+//     token=req.header('x-auth-token');
+//     verified=jwt.verify(token,"passwordKey");
+//     const user = await User.findById(verified.id);
+//     res.json({ ...user._doc, token });
+// });
 module.exports=authRouter; 
